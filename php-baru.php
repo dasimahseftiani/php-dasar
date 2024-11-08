242a14a643efe79777681f6fe90331a10db7e551
dasimahseftiani <Seftianidsmh@gmail.com>
2024-11-08 15:35:05 +0700

    first commit

---
+++ php-baru.php
@@ -0,0 +1,43 @@
+<?php
+
+echo " Data Pribadi";
+
+const Nama = "Dasimah Seftiani";
+const JK = "P";
+$umur ="22"
+
+
+?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+	
+ </head>
+ <body>
+	
+       <table border = "1" cellspacing = "0" cellpadding="5px">
+				<tr>
+					<th>Nama</th>
+					<td>:</td>
+		<td> <?php echo Nama?> </td>
+	</tr>
+	<tr>
+		<th>Umur</th>
+		<td>:</td>
+		<td> <?php echo $umur?> </td>
+	</tr>
+
+		<th>Alamat</th>
+		<td>:</td>
+		<td>Cihanjuang</td>
+	</tr>
+
+		<th>Tinggi Badan </th>
+		<td>:</td>
+		<td> 160 cm </td>
+		
+			
+</tr>
+</body>
+ </html>
